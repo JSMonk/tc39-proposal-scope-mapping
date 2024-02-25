936cd956495cb7c28036647e00493e7adde594b1
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-02-25 17:53:02 +0100

    Remove .ts files

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { getOriginalFrames } from "./getOriginalFrames";
-export { decodeGeneratedScopes, decodeOriginalScopes } from "./decodeScopes";
-export { encodeGeneratedScopes, encodeOriginalScopes } from "./encodeScopes";
-
-export type * from "./types"
